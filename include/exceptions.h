705cef3275d14872b524d6c51f17a1f3198e907a
Alexis Vandewalle <alexivande60@gmail.com>
2026-01-16 15:23:41 +0100

    Sprint1.5 - Add main menu to the GUI

---
+++ include/exceptions.h
@@ -0,0 +1,30 @@
+// AppException.h
+#ifndef EXCEPTIONS_H // include guard
+#define EXCEPTIONS_H
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace soko
+{
+    /**
+     * base class for exception handling in sokoban App
+     */
+    class AppException : public exception
+    {
+        private:
+        public:
+    };
+
+    class FileNotFoundException : public AppException
+    {
+        private:
+            string filePath;
+        public:
+            FileNotFoundException(string filePathAttr) : filePath(filePathAttr){}
+            string getFilePath(){ return filePath; }
+    };
+}
+
+#endif /* EXCEPTIONS_H */
